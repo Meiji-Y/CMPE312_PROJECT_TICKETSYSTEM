57ce36e59d2451d19971a74114de4f5000b23696
Muhlis Erdem Yıldız <muhliserdemyildiz@gmail.com>
2023-05-06 22:31:47 +0300

    Proje dosyası ekle.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace CMPE312_PROJECT_TICKETSYSTEM
+{
+    /// <summary>
+    /// MainWindow.xaml etkileşim mantığı
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            SqlConnection sqlConnection;
+            InitializeComponent();
+            string connectionString = ConfigurationManager.ConnectionStrings["CMPE312_PROJECT_TICKETSYSTEM.Properties.Settings.TicketSystemDBConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
